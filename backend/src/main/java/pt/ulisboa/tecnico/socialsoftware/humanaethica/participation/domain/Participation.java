20ac9cf9a7420ca05332fa003747110c88071c5b
Pedro Sousa <sousaampedro@gmail.com>
2024-02-22 21:36:10 +0000

    feat(participation): create domain class(#18)

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/domain/Participation.java
@@ -0,0 +1,74 @@
+package pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.domain;
+
+import jakarta.persistence.*;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Volunteer;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "participation")
+public class Participation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String rating;
+
+    private LocalDateTime acceptanceDate;
+
+    @ManyToOne
+    private Volunteer volunteer;
+
+    @ManyToOne
+    private Activity activity;
+
+    public Participation() {
+    }
+
+    public Participation(Activity activity, Volunteer volunteer/*, ParticipationDto participationDto*/) {
+        //setRating(participationDto.getRating());
+        setActivity(activity);
+        setVolunteer(volunteer);
+        setAcceptanceDate(LocalDateTime.now());
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public LocalDateTime getAcceptanceDate() {
+        return acceptanceDate;
+    }
+
+    public void setAcceptanceDate(LocalDateTime acceptanceDate) {
+        this.acceptanceDate = acceptanceDate;
+    }
+
+    public Activity getActivity() {
+        return activity;
+    }
+
+    public void setActivity(Activity activity) {
+        this.activity = activity;
+        activity.addParticipation(this);
+    }
+
+    public Volunteer getVolunteer() {
+        return volunteer;
+    }
+
+    public void setVolunteer(Volunteer volunteer) {
+        this.volunteer = volunteer;
+        volunteer.addParticipation(this);
+    }
+}
