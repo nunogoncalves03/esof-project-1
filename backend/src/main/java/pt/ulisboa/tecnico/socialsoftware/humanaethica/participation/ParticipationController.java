4d6a86f7108aedf8bbfb64f97e12a60d0f4ceb23
Pedro Sousa <pedro.sousa.21@tecnico.ulisboa.pt>
2024-03-04 21:16:19 +0000

    feat(participation): create ParticipationController(#23)

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/ParticipationController.java
@@ -0,0 +1,24 @@
+package pt.ulisboa.tecnico.socialsoftware.humanaethica.participation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.dto.ParticipationDto;
+
+@RestController
+@RequestMapping("/participation")
+public class ParticipationController {
+    @Autowired
+    private ParticipationService participationService;
+
+    private static final Logger logger = LoggerFactory.getLogger(ParticipationService.class);
+
+    @PostMapping("/{activityId}")
+    @PreAuthorize("hasRole('ROLE_MEMBER')")
+    public ParticipationDto createParticipation(@PathVariable Integer activityId, @Valid @RequestBody ParticipationDto participationDto) {
+        return participationService.createParticipation(activityId, participationDto);
+    }
+}
