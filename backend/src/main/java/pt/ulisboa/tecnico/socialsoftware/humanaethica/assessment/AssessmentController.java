0e2eab07942712f1c9a66d4f985802bd5cfafca3
João Gouveia <joaotalonegouveia@tecnico.ulisboa.pt>
2024-03-04 13:18:30 +0000

    feat(assessment): create the AssessmentController class and implement the post request mapping (#52)

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/assessment/AssessmentController.java
@@ -0,0 +1,29 @@
+package pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment.dto.AssessmentDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.auth.domain.AuthUser;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/assessments")
+public class AssessmentController {
+    @Autowired
+    private AssessmentService assessmentService;
+
+    private static final Logger logger = LoggerFactory.getLogger(AssessmentController.class);
+
+    @PostMapping("/{institutionId}")
+    @PreAuthorize("(hasRole('ROLE_VOLUNTEER'))")
+    public AssessmentDto registerAssessment(Principal principal, @PathVariable int institutionId, @Valid @RequestBody AssessmentDto assessmentDto){
+        int volunteerId = ((AuthUser) ((Authentication) principal).getPrincipal()).getUser().getId();
+        return assessmentService.registerAssessment(institutionId, volunteerId, assessmentDto);
+    }
+}
