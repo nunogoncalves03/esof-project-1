04ebb9c9794313e4782bfe589af5f9bc8a6ede7c
Pedro Sousa <pedro.sousa.21@tecnico.ulisboa.pt>
2024-03-06 14:47:55 +0000

    feat(participation): List participations by activity controller (#91)

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/ParticipationController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/ParticipationController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.dto.ParticipationDto;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/participations")
 public class ParticipationController {
@@ -15,6 +17,12 @@ public class ParticipationController {
     private ParticipationService participationService;
 
     private static final Logger logger = LoggerFactory.getLogger(ParticipationService.class);
+
+    @GetMapping("/{activityId}")
+    @PreAuthorize("hasRole('ROLE_MEMBER') and hasPermission(#activityId, 'ACTIVITY.MEMBER')")
+    public List<ParticipationDto> getActivityParticipations(@PathVariable Integer activityId) {
+        return participationService.getParticipationsByActivity(activityId);
+    }
 
     @PostMapping("/{activityId}")
     @PreAuthorize("hasRole('ROLE_MEMBER')")
