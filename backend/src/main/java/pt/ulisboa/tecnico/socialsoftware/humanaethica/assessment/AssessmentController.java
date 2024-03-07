07bd2c6e767969072be7670167fdacd45b26eee4
Gonçalo Rua <goncalonrua@tecnico.ulisboa.pt>
2024-03-07 19:04:50 +0000

    feat(assessment): implement assessment GET controller (#38)

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/assessment/AssessmentController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/assessment/AssessmentController.java
@@ -1,16 +1,17 @@
 package pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment;
 
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment.dto.AssessmentDto;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.auth.domain.AuthUser;
 
 import java.security.Principal;
+import java.util.List;
 
 @RestController
 @RequestMapping("/assessments")
@@ -19,6 +20,11 @@ public class AssessmentController {
     private AssessmentService assessmentService;
 
     private static final Logger logger = LoggerFactory.getLogger(AssessmentController.class);
+
+    @GetMapping("{institutionId}")
+    public List<AssessmentDto> getInstitutionAssessments(@PathVariable Integer institutionId) {
+        return assessmentService.getAssessmentsByInstitution(institutionId);
+    }
 
     @PostMapping("/{institutionId}")
     @PreAuthorize("(hasRole('ROLE_VOLUNTEER'))")
